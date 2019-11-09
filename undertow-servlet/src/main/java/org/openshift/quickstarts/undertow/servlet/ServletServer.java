0cac3aaa5a1bbeb4e80da3535c7db5a7c99e6c96
rgupta1234 <rkgupta75@gmail.com>
2019-11-09 08:57:59 -0600

    Update ServletServer.java

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -67,7 +67,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Blue: " + ip)
+                                    .addInitParam("message", "Bluex: " + ip)
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
